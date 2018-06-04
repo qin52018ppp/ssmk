c164561eebd80fdb815ed19dc69553a664288a36
qin52018ppp <qin52018love@163.com>
2018-06-04 15:05:19 +0800

    redis 深入研究

--- ssmk_dubbox_service/src/main/java/com/baobao/ssmk/Main.java
+++ ssmk_dubbox_service/src/main/java/com/baobao/ssmk/Main.java
@@ -1,9 +1,6 @@
 package com.baobao.ssmk;/**
  * Created by Administrator on 2018/5/30.
  */
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * @Company:新概念保险
  * @Auth:秦盼（Q）
@@ -13,9 +10,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main {
     public static void main(String[] args) {
-/*        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[]{"classpath*:META-INF/spring/applictionContext.xml"});
-        context.start();*/
         com.alibaba.dubbo.container.Main.main(args);
     }
 }
